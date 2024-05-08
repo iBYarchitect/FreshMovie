4265f4f59e06de7a84409867cdf94cf86e0cdb44
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2024-05-08 15:02:26 +0200

    Initial commit

---
+++ FreshMovie/FreshMovie/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FreshMovie
+//
+//  Created by Alex Zavadskiy on 8.05.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FreshMovie/FreshMovie/FreshMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  FreshMovieApp.swift
+//  FreshMovie
+//
+//  Created by Alex Zavadskiy on 8.05.24.
+//
+
+import SwiftUI
+
+@main
+struct FreshMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
