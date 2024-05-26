0b03082675d054c89fc2deccc8df4f3ea6ef174a
Alex Zavadskiy <103530508+iBYarchitect@users.noreply.github.com>
2024-05-26 17:12:24 +0200

    Added Movie, Genre, ProductionCompany, ProductionCountry structs

---
+++ FreshMovie/FreshMovie/Model/Genre.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct Genre: Codable {
+    /// The unique identifier of the genre.
+    let id: Int
+
+    /// The name of the genre.
+    let name: String
+}
---
+++ FreshMovie/FreshMovie/Model/ProductionCountry.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+/// Country where a movie is produced.
+struct ProductionCountry: Codable {
+    /// The name of the country.
+    let name: String
+}
